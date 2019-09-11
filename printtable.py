eaa9f345e02730501c5af262d68b8c2f788ee1d8
sofiapapav <55189852+sofiapapav@users.noreply.github.com>
2019-09-11 14:45:06 +0200

    A function calculating the power has been added.

--- printtable.py
+++ printtable.py
@@ -3,3 +3,14 @@ def multtable(number, limit):
         print(i * number)
 
 multtable(7, 10)
+
+
+def powertable(power, limit):
+	for n in range(1, limit + 1):
+		result =  n **power
+		print(result)
+
+powertable(2, 4)
+powertable(3, 3)
+
+
