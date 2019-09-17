5c795c2fefaee9279c83bd0de62d3606dd73f566
Olav Vahtras <vahtras@kth.se>
2019-09-17 13:37:11 +0200

    use "if __name..." for better import

--- printtable.py
+++ printtable.py
@@ -2,7 +2,6 @@ def multtable(number, limit):
     for i in range(1, limit+1):
         print(i * number)
 
-multtable(7, 10)
 
 
 def powertable(power, limit):
@@ -10,7 +9,8 @@ def powertable(power, limit):
 		result =  n **power
 		print(result)
 
-powertable(2, 4)
-powertable(3, 3)
+if __name__ == "__main__":
+    multtable(7, 10)
+    powertable(2, 4)
 
 
